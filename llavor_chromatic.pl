cfcd3c909df6984593f3bf4c089c6b6786a167a6
Lluís Trilla <clank201@gmail.com>
2020-06-07 19:36:36 +0200

    Afegits comentaris

--- llavor_chromatic.pl
+++ llavor_chromatic.pl
@@ -24,123 +24,118 @@ sat(CNF,I,M):-
 %  - si hi ha una clausula unitaria sera aquest literal, sino
 %  - un qualsevol o el seu negat.
 % ...
-
 % Ha de triar un literal d’una clausula unitaria, si no n’hi ha cap, llavors un literal pendent qualsevol.
-tria(CNF, Lit):- member([Lit], CNF),!.
-tria([[Lit|_]|_],Lit).
-tria([[Lit|_]|_],L):- L is -Lit.
+tria(CNF, Lit):- member([Lit], CNF),!. %triem literal de clausual unitaria
+tria([[Lit|_]|_],Lit). %en triem un altre qualsevol
+tria([[Lit|_]|_],L):- L is -Lit. %provem de triar-lo negat
 
 %%%%%%%%%%%%%%%%%%%%%
-% simlipf(Lit, F, FS)
+% simplif(Lit, F, FS)
 % Donat un literal Lit i una CNF,
 % -> el tercer parametre sera la CNF que ens han donat simplificada:
 %  - sense les clausules que tenen lit
 %  - treient -Lit de les clausules on hi es, si apareix la clausula buida fallara.
-% ...
-
 simplif(_,[],[]).
-simplif(Lit,[X|CNF],[Y|F]):-   NL is -Lit, %negat del literal
-                                member(NL, X),
-                                delete(X, NL, Y), %eliminem element de la llista
-                                \+ Y = [],
+simplif(Lit,[C|CNF],[C2|F]):-   Negat is -Lit, %eliminem el literal de les clàusules on surti negat, si la clàusula queda buida és que no el podem triar.
+                                member(Negat, C),
+                                delete(C, Negat, C2),
+                                \+ C2 = [],
                                 simplif(Lit, CNF, F).
 
-simplif(Lit,[X|CNF],[X|F]):-   \+ member(Lit, X),
-                                NL is -Lit,
-                                \+ member(NL, X), %si no esta a la clausula l'afegim
+simplif(Lit,[C|CNF],[C|F]):-   \+ member(Lit, C), %simplifiquem en cas que el literal no surti en la clàusula
+                                Negat is -Lit,
+                                \+ member(Negat, C),
                                 simplif(Lit, CNF, F).
 
-simplif(Lit,[X|CNF],F):-       member(Lit, X), %simplifiquem si el literal esta a la clausula
+simplif(Lit,[C|CNF],F):-       member(Lit, C), %simplifiquem si el literal esta com a clausula unitaria
                                 simplif(Lit, CNF, F).
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 %%%%%%%%%%%%%%%%%%%
 % unCert(L,CNF)
 % Donat una llista de variables booleanes,
 % -> el segon parametre sera la CNF que codifica que exactament una sigui certa.
-% ... pots crear i utilitzar els auxiliars comaminimUn i nomesdUn
-
 unCert(L, CNF):- comaminimUn(L, CNF1), nomesdUn(L, CNF2), append(CNF1, CNF2, CNF).
 
 %%%%%%%%%%%%%%%%%%%
 % comaminimUn(L,CNF)
 % Donat una llista de variables booleanes,
 % -> el segon parametre sera la CNF que codifica que com a minim una sigui certa.
-% ...
 comaminimUn(L,[L]).
+
 %%%%%%%%%%%%%%%%%%%
 % nomesdUn(L,CNF)
 % Donat una llista de variables booleanes,
 % -> el segon parametre sera la CNF que codifica que com a molt una sigui certa.
-% ...
-
 nomesdUn(L, CNF):- llistaNegativa(L,Ns), combinacions(Ns,CNF).
 
+%%%%%%%%%%%%%%%%%%%
+% combinacions(LL,Res)
+% Donada una llista de variables, retorna una llista amb totes les combinacions possibles entre elles
 combinacions([],[]):-!.
 combinacions([A|AS],Res):- emparellar(A,AS,R1), append(R1,R2,Res), combinacions(AS,R2).
 
+%%%%%%%%%%%%%%%%%%%
+% emparellar(V,Llista,Sortida)
+% Donada una variable i una llista de variables, retorna tots els emparellaments entre V i els valors de la Llista
 emparellar(_,[],[]):-!.
 emparellar(A,[B|BS],[[A,B]|LS]):- dif(A,B), emparellar(A,BS,LS).
 
+%%%%%%%%%%%%%%%%%%%
+% llistaNegativa(LL,Res)
+% Nega tots els valors de la llista.
 llistaNegativa([],[]):-!.
 llistaNegativa([A|AS], [X|XS]):- X is -A, llistaNegativa(AS,XS).
 
-
-% AUX
+%%%%%%%%%%%%%%%%%%%
 % llista(I,F,L)
 % Donat un inici i un fi
-% -> el tercer parametre sera una llista de numeros d'inici a fi
+% Equivalent a un range de python, retorna una llista de valors de I fins a F
 llista(X,X,[X]) :- !. %en cas que siguin iguals I i F
 llista(X,Y,[X|Xs]) :-%concatenem x a xs
     X =< Y,%X ha de ser mes petit
     Z is X+1,%sumem 1
     llista(Z,Y,Xs).%next
 
+%%%%%%%%%%%%%%%%%%%
 % llistaDeLlistes(N,M,L)
 % donats N i M, retorna una llista de N llistes de tamany M amb nombres del 1 al N*M
-% -> L sera la llista a retornar
 llistaDeLlistes(N,M,L) :- llistaDeLlistesAux(N,M,1,L).
 llistaDeLlistesAux(1,M,X,R) :- T is X+M-1, llista(X,T,L),R = [L],!.
 llistaDeLlistesAux(N,M,X,L) :- T is X+M-1,llista(X,T,Laux), V1 is N - 1, V2 is X+M, llistaDeLlistesAux(V1,M,V2,Lrec), append([Laux],Lrec,L).
 
-
-% Filtra els numeros negatius
+%%%%%%%%%%%%%%%%%%%
+% negative(N)
+% Filtra els nombres negatius
 negative(Number) :-
    Number < 0.
 
+%%%%%%%%%%%%%%%%%%%
+% restriccioColorPerNode(LL,CNF) 
+% Donat una llista de llistes de variables, retorna una CNF la qual conté la codificació del color únic assignat a cada node del graf.
 restriccioColorPerNode([],[]):-!.
 restriccioColorPerNode([N|G],CNF):-
    unCert(N,Resultat),
    restriccioColorPerNode(G,NovaCNF),
    append(NovaCNF,Resultat,CNF).
 
+%%%%%%%%%%%%%%%%%%%
+% buscarPos(N,M)
+% Donat un número N i una llista M, retorna el valor de la posició N de la llista M.
 buscarPos(1,[X|_],X).
 buscarPos(K, [_|L],X) :- buscarPos(K1, L, X), K is K1 + 1.
 
-%restriccioColorsFixes(Graf,[R|Inici],[]).%WIP
+%%%%%%%%%%%%%%%%%%%
+% restriccioColorsFixes(Graf,[R|Inici],[])
+% Donat el graf (llista de llistes de vars) i una llista de colors fixes, retorna una CNF la qual codifica que cada node del graf especificat tingui el color fixe indicat.
 restriccioColorsFixes(_,[],[]):-!.
 restriccioColorsFixes(_,[(N,C)|Inici],CNF):-
    R is N * C,
    restriccioColorsFixes([],Inici,NovaCNF),
    append(NovaCNF,[[R]],CNF).
 
-% Graf
-% [ (4 nodes, 3 colors)[1,5,7]
-%  (node1)[1(blau),2(verd),3(vermell)]
-%  (node2)[4(blau),5(verd),6(vermell)]
-%  (node3)[7(blau),8(verd),9(vermell)]
-%  (node4)[10(blau),11(verd),12(vermell)]
-% ]
-
+%%%%%%%%%%%%%%%%%%%
+% restriccioColorsAdjacentsAux(Node1, Node2,CNF)
+% Donades dues llistes de variables (2 nodes del graf), codifica la CNF que fa que els 2 nodes no tinguin el mateix color
 restriccioColorsAdjacentsAux([E],[D],CNF):-
    nomesdUn([E,D],CNF).
 restriccioColorsAdjacentsAux([E|Ex],[D|Dx],CNF):-
@@ -148,7 +143,9 @@ restriccioColorsAdjacentsAux([E|Ex],[D|Dx],CNF):-
    restriccioColorsAdjacentsAux(Ex,Dx,NovaCNF),
    append(NovaCNF,C,CNF).
 
-
+%%%%%%%%%%%%%%%%%%%
+% restriccioColorsAdjacents(Graf,[R|Inici],[])
+% Donat el graf (llista de llistes de vars), retorna una CNF que dos nodes connectats per una aresta no continguin el mateix color.
 restriccioColorsAdjacents(_,[],[]):-!.
 restriccioColorsAdjacents(Graf,[(E,D)|Arestes],CNF):-
    buscarPos(E,Graf,NodeE),
@@ -156,10 +153,6 @@ restriccioColorsAdjacents(Graf,[(E,D)|Arestes],CNF):-
    restriccioColorsAdjacentsAux(NodeE,NodeD,C),
    restriccioColorsAdjacents(Graf,Arestes,NovaCNF),
    append(C,NovaCNF,CNF).
-
-
-%noAmenacesColumnes(N,D):-
-%   columnes(N,L), llistesAVars(L,N,VARS), noAmenacesAux(VARS,D).
 
 
 %%%%%%%%%%%%%%%%%%%
@@ -175,47 +168,36 @@ codifica(N,K,Arestes,Inici,C):-
    restriccioColorsAdjacents(Graf,Arestes,CNF3),
    append(CNF1,CNF2,CNF4),
    append(CNF3,CNF4,C).
-   
-
-  %crear la llista de llistes de variables pels colors de cada node
-  %crear la CNF que fa que cada node tingui un color
-  %crear la CNF que força els colors dels nodes segons Inici
-  %crear la CNF que fa que dos nodes que es toquen tinguin colors diferents
-  %C sera el resultat d'ajuntar les CNF creades
-
     
                                  
 %%%%%%%%%%%%%%%%%%%%
-% resolGraf(N,A,K,Inputs)
-% Donat el nombre de nodes, el nombre de colors, les Arestes A, i les inicialitzacions,
-% -> es mostra la solucio per pantalla si en te o es diu que no en te.
+% mostrarSolucio(Solucio, Color,CMAX)
+% Donada una solució, el nombre màxim de colors, i el color pel que anem, mostra per pantalla la pertanyença dels nodes de la solució al color actual i als seguents.
 mostrarSolucio(_,Color,CMAX):-
    Color is CMAX + 1,!.
 mostrarSolucio(Sol,Color,CMAX):-
    NextColor is Color + 1,
-   mostrarNodesColor(Sol,Color,CMAX,L),nl,
+   obtenirNodesColor(Sol,Color,CMAX,L),nl,
    write('color '),write(Color),write(': '),write(L),
    mostrarSolucio(Sol,NextColor,CMAX),!.
 
-mostrarNodesColor([],_,_,[]):-!.
-mostrarNodesColor([V|Vl],Color,CMAX,L):-
+%%%%%%%%%%%%%%%%%%%%
+% obtenirNodesColor(Sol,Color,CMAX,L)
+% Donada una solució al problema, el Color i el nombre màxim de colors, retorna una llista amb els nodes que pertanyen al Color.
+obtenirNodesColor([],_,_,[]):-!.
+obtenirNodesColor([V|Vl],Color,CMAX,L):-
    T1 is V - 1,
    T2 is T1 mod CMAX, % per obtenir el color
    Color is T2 + 1,
    Node is truncate((T1 / CMAX) + 1),
-   mostrarNodesColor(Vl,Color,CMAX,L1),
+   obtenirNodesColor(Vl,Color,CMAX,L1),
    append([Node],L1,L),!.
-mostrarNodesColor([_|Vl],Color,CMAX,L):-
-   mostrarNodesColor(Vl,Color,CMAX,L).
+obtenirNodesColor([_|Vl],Color,CMAX,L):-
+   obtenirNodesColor(Vl,Color,CMAX,L).
 
-
-%color 1: 1, 4, 7, 8
-%color 2: 2, 5, 6
-%color 3: 3, 9, 10
-
-list_length([],0).
-list_length([_|XS],N):-list_length(XS,N1), N is N1+1.
-
+%%%%%%%%%%%%%%%%%%%%
+% trobarCromatics(N,K,A,I)
+% Resol i mostra el problema de coloritzar un graf, (backtrackejable)
 trobarCromatics(N,K,A,I):-
    codifica(N,K,A,I,CNF),
    sat(CNF,[],RESULTAT),
@@ -223,22 +205,21 @@ trobarCromatics(N,K,A,I):-
    sort(ResultatMostrar,ResultatMostrarOrdenat),
    mostrarSolucio(ResultatMostrarOrdenat,1,K).
    
+%%%%%%%%%%%%%%%%%%%%
+% trobarNombreCromatic(N,K,A,I)
+% Retorna el nombre mínim de colors per pintar el graf.
 trobarNombreCromatic(N,A,I,K,K):- codifica(N,K,A,I,CNF),sat(CNF,[],_),!.
 trobarNombreCromatic(N,A,Inputs,K,S):- NextK is K + 1, trobarNombreCromatic(N,A,Inputs,NextK,S).
 
 
 %%%%%%%%%%%%%%%%%%%%
-% chromatic(N,A,Inputs)
+% resol(N,A,Inputs)
 % Donat el nombre de nodes,  les Arestes A, i les inicialitzacions,
 % -> es mostra la solucio per pantalla si en te o es diu que no en te.
 % Pista, us pot ser util fer una immersio amb el nombre de colors permesos.
 resol(N,A,Inputs):- trobarNombreCromatic(N,A,Inputs,1,K),write('El nombre cromàtic és '), write(K),nl,trobarCromatics(N,K,A,Inputs), true.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% com a query podeu cridar:
-% ?- graf1(N,A), chromatic(N,A,[]).
-% i aixi amb altres grafs que us definiu com els que hi ha a continuacio:
    
 
 % aquest graf te 3 nodes i nombre chromatic 3.
@@ -255,9 +236,7 @@ graf2(23,[(1,2),(1,4),(1,7),(1,9),(1,13),(1,15),(1,18),(1,20),(2,3),(2,6),(2,8),
          (8,11),(8,13),(8,16),(8,22),(9,11),(9,12),(9,16),(9,22),(10,11),(10,14),(10,15),(10,22),
          (11,17),(11,18),(11,19),(11,20),(11,21),(12,23),(13,23),(14,23),(15,23),(16,23),(17,23),
          (18,23),(19,23),(20,23),(21,23),(22,23)]).
-
-
-      
+ 
 graf3(25,
       [(1,7),(1,5),(1,6),(1,11),(1,16),(1,21),(2,8),(2,14),(2,20),(2,6),(2,3),(2,4),(2,5),(2,7),(2,1),
       (3,9),(3,15),(3,7),(3,2),(3,1),(4,10),(4,8),(4,12),(4,16),(4,5),(4,9),(4,14),(4,19),
